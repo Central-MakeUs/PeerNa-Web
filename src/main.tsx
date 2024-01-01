0baae4959d9d59f705f7dc9e382a80ae5c98f6ab
mj <minai621@naver.com>
2024-01-02 01:46:45 +0900

    feat: prettier 설치 및 설정 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { DEVELOPMENT, MODE } from "./constants/environments.ts";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { DEVELOPMENT, MODE } from './constants/environments.ts';
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       {MODE === DEVELOPMENT && <ReactQueryDevtools initialIsOpen={false} />}
       <App />
     </QueryClientProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="vite/client" />
 
-import { ModeTypes } from "./constants/environments";
+import { ModeTypes } from './constants/environments';
 
 interface ImportMeta {
   env: ImportMetaEnv;
