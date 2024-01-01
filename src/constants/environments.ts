f789b058bb09beae2f27e6af7c5cde1a52465ff6
mj <minai621@naver.com>
2024-01-02 01:31:06 +0900

    feat: @tanstack/react-query와 devtools 설치 및 설정 적용

---
+++ src/constants/environments.ts
@@ -0,0 +1,4 @@
+export const DEVELOPMENT = "development";
+export const PRODUCTION = "production";
+export type ModeTypes = typeof DEVELOPMENT | typeof PRODUCTION;
+export const MODE: ModeTypes = import.meta.env.MODE as ModeTypes;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { DEVELOPMENT, MODE } from "./constants/environments.ts";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      {MODE === DEVELOPMENT && <ReactQueryDevtools initialIsOpen={false} />}
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
