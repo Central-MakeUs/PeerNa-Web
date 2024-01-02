9f6062b092c66b8fb6da4da3d3327c66f61f9a4b
mj <minai621@naver.com>
2024-01-03 01:02:40 +0900

    feat: foundation의 color theme 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary01: '#475AFF',
+        primary02: '#253AF4',
+        'secondary-purple': '#A39EFF',
+        'secondary-pink': '#FF92FC',
+        'secondary-orange': '#FF6E22',
+        'secondary-yellow': '#FFC31D',
+        white: '#FFFFFF',
+        gray01: '#E6E8EB',
+        gray02: '#E6E8EB',
+        gray03: '#C8CBD0',
+        gray04: '#868C92',
+        gray05: '#565D63',
+        gray06: '#353D43',
+        gray07: '#2C333A',
+        gray08: '#0D1326',
+        black: '#000000',
+        danger01: '#FF7474',
+        danger02: '#A64141',
+      },
+    },
   },
   plugins: [],
 };
