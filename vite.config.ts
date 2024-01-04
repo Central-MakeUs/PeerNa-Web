f0e8032f7a1d8746d3323dea5e7d3c8fc7d885d7
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2024-01-04 23:05:56 +0900

    chore: svg 세팅 및 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {},
+    }),
+  ],
   resolve: {
     alias: [
       { find: '@constants', replacement: '/src/constants' },
       { find: '@components', replacement: '/src/components' },
+      { find: '@components', replacement: '/src/assets' },
       { find: '@', replacement: '/src' },
     ],
   },
