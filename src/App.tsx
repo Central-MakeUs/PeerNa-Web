50318a866eb7f6319ccabf52f3f962a75f8b7b21
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2024-01-09 23:57:30 +0900

    feat: GlobalSprite 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Stack } from '@components/common/atom/StackFlow';
+import { GlobalSprite } from '@assets/GlobalSprite';
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       {/* Notice)Stack은 StackFlow의 진입점입니다. */}
       {/*         react-router-dom의 createBrowseRouter */}
       <Stack />
+      <GlobalSprite />
     </div>
   );
 }
