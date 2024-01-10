f8a35be70d06cb3053c7dd3d3c408f73d272c68f
mj <minai621@naver.com>
2024-01-11 02:18:09 +0900

    feat: icon 수정

--- src/assets/SvgIcon.tsx
+++ src/assets/SvgIcon.tsx
@@ -1,21 +1,23 @@
 import SpriteIcon from '@assets/SpriteIcon.svg';
 import { Icon, IconId } from '@constants/icons';
+import { Palette } from '@constants/styles';
+import { ColorTypes } from '@type/styles';
 
 interface SvgIconProps {
   id: IconId;
   width?: number;
   height?: number;
-  color?: string;
+  color?: ColorTypes;
 }
 
 const SvgIcon = ({
   id,
   width = 24,
-  height = 20,
-  color = 'currentColor',
+  height = 24,
+  color = 'gray02',
 }: SvgIconProps) => {
   return (
-    <svg width={width} height={height} fill={color}>
+    <svg width={width} height={height} className={`text-[${Palette[color]}]`}>
       <use href={`${SpriteIcon}#${Icon[id]}`} />
     </svg>
   );
