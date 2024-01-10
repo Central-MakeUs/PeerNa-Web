99e36fa10e5e2936ab4099cda3f3b17af3185f40
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2024-01-10 23:17:26 +0900

    rename: SvgIcon assets으로 폴더 구조 변경

---
+++ src/assets/SvgIcon.tsx
@@ -0,0 +1,24 @@
+import SpriteIcon from '@assets/SpriteIcon.svg';
+import { Icon, IconId } from '@constants/icons';
+
+interface SvgIconProps {
+  id: IconId;
+  width?: number;
+  height?: number;
+  color?: string;
+}
+
+const SvgIcon = ({
+  id,
+  width = 24,
+  height = 20,
+  color = 'currentColor',
+}: SvgIconProps) => {
+  return (
+    <svg width={width} height={height} fill={color}>
+      <use href={`${SpriteIcon}#${Icon[id]}`} />
+    </svg>
+  );
+};
+
+export default SvgIcon;
