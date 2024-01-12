830d6441621d1f7c3366da8019b144d982345991
mj <minai621@naver.com>
2024-01-13 03:49:10 +0900

    feat: RequestListItem 구현

--- src/components/common/atom/ProjectListItem.tsx
+++ src/components/common/atom/ProjectListItem.tsx
@@ -14,7 +14,7 @@ export default function ProjectListItem({
   buttons,
 }: ProjectListItemProps) {
   return (
-    <div className="flex justify-between w-[350px] h-[44px] px-5 py-4 box-content">
+    <div className="flex justify-between items-center w-[350px] h-[44px] px-5 py-4 box-content">
       <div className="flex flex-col gap-1">
         <Typography variant="body01">{title}</Typography>
         <Typography variant="body05" className={`text-[${Palette.gray05}]`}>
