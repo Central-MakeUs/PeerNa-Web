1a85df2ca7f567e964538f67710e1813d1d207f1
mj <minai621@naver.com>
2024-01-13 17:04:45 +0900

    feat: test api 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import { DEVELOPMENT, MODE } from '@constants/environments.ts';
 import '@constants/trash.ts';
 import { NextUIProvider } from '@nextui-org/react';
 import '@stackflow/plugin-basic-ui/index.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -10,7 +14,23 @@ import { RecoilRoot } from 'recoil';
 import App from './App.tsx';
 import './index.css';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    // TODO: mutation 에러에 대해서 핸들링 해야 합니다. toast를 띄우는 것을 고려하고 있습니다.
+    mutations: {
+      onError: error => console.log(error),
+    },
+  },
+  // TODO: queries 에러에 대해서 핸들링 해야 합니다.
+  queryCache: new QueryCache({
+    onError: (error, query) => {
+      if (query.meta && query.meta.errorMessage) {
+        console.error('error message', query.meta.errorMessage);
+        // toast(() => query.meta.errorMessage);
+      }
+    },
+  }),
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
