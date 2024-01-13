eb6b97a78f835482b649c48b2b6a7e7641f24d83
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2024-01-14 00:07:56 +0900

    feat: 아이콘 및 텍스트 커스텀 및 date 포맷 변경

--- src/components/common/atom/DatePicker.tsx
+++ src/components/common/atom/DatePicker.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import { CalendarIcon } from '@radix-ui/react-icons';
 import { format } from 'date-fns';
 
 import { cn } from '@utils/shadcn';
 import { Calendar } from '@components/common/atom/calendar';
+import Typography from '@components/common/atom/Typography';
+import SvgIcon from '@components/common/atom/SvgIcon';
 import { Button } from '@components/common/atom/calendarButton';
 import {
   Popover,
@@ -20,12 +21,16 @@ const DatePicker = () => {
         <Button
           variant={'outline'}
           className={cn(
-            'w-[240px] justify-start text-left font-normal',
+            '!w-[161px] !h-[52px] gap-[11px] justify-start font-normal !text-[16px] bg-[#F6F7F8] border-1 border-[#E3E6E8] rounded-xl',
             !date && 'text-muted-foreground',
           )}
         >
-          <CalendarIcon className="mr-2 h-4 w-4" />
-          {date ? format(date, 'PPP') : <span>Pick a date</span>}
+          <SvgIcon id="Calendar" width={18} height={18} color="gray07" />
+          {date ? (
+            format(date, 'yyyy.MM.dd')
+          ) : (
+            <Typography variant={'body02'}>시작일</Typography>
+          )}
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0" align="start">
