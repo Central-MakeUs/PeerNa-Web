469b6abc830bd40e7ae4a92828ea16264cd763fb
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2024-01-13 23:28:53 +0900

    feat: FullBleed 컴포넌트 구현 (#6)

---
+++ src/components/common/atom/FullBleed.tsx
@@ -0,0 +1,26 @@
+import Button from '@components/common/atom/Button';
+import Typography from './Typography';
+import SvgIcon from '@components/common/atom/SvgIcon';
+
+const FullBleed = () => {
+  const handleClick = () => {
+    console.log('클릭');
+  };
+  return (
+    <div className="w-[390px] border-t-1 border-[#E2E6E8] flex justify-center">
+      <Button buttonVariant="naked" onClick={handleClick} className="w-[390px]">
+        <Typography
+          as="span"
+          variant={'body05'}
+          fontColor="gray08"
+          className="mr-1"
+        >
+          더보기
+        </Typography>
+        <SvgIcon id="ArrowRight" width={6} height={12} color="gray07" />
+      </Button>
+    </div>
+  );
+};
+
+export default FullBleed;
