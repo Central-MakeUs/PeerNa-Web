ce452646ea98d4f5580b1c62439433744356f897
mj <minai621@naver.com>
2024-01-13 14:58:34 +0900

    feat: axios instance 설치 및 설정 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-
+import mkcert from 'vite-plugin-mkcert';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   resolve: {
     alias: [
       { find: '@constants', replacement: '/src/constants' },
@@ -12,6 +12,7 @@ export default defineConfig({
       { find: '@type', replacement: '/src/type' },
       { find: '@utils', replacement: '/src/utils' },
       { find: '@store', replacement: '/src/store' },
+      { find: '@apis', replacement: '/src/apis' },
       { find: '@', replacement: '/src' },
     ],
   },
