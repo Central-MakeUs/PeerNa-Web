4182d21b5d11f93d2cb3b7244e05c225f481fbe8
mj <minai621@naver.com>
2024-01-14 22:55:52 +0900

    refac: AvartarButton 리팩토링

--- src/components/common/atom/AvatarButton.tsx
+++ src/components/common/atom/AvatarButton.tsx
@@ -1,3 +1,4 @@
+import { SvgIconProps } from '@components/common/atom/SvgIcon';
 import useFocusTimeout from '@hooks/useFocusTimeout';
 import IconButton from './IconButton';
 
@@ -7,39 +8,27 @@ type AvatarButtonProps = {
 
 const AvatarButton = ({ type }: AvatarButtonProps) => {
   const { isFocused, handleFocus } = useFocusTimeout();
+  const iconProps: SvgIconProps = {
+    id: 'Person',
+    width: 28,
+    height: 28,
+    color: 'gray04',
+  };
+  const getBackgroundColor = () => {
+    if (type === 'default') return isFocused ? 'bg-[#E3E6E8]' : 'bg-[#F6F7F8]';
+    if (type === 'orange') return isFocused ? 'bg-[#E85830]' : 'bg-[#FF744D]';
+  };
+  const backgroundColor = getBackgroundColor();
+  const commonIconButtonClasses = `${backgroundColor} w-[68px] h-[68px] flex justify-center items-center 
+  rounded-[100px] border-1 border-[#E3E6E8]`;
 
   return (
     <div className="flex justify-center items-center shadow-default !w-[72px] !h-[72px] rounded-full">
-      {type === 'default' && (
-        <IconButton
-          onClick={handleFocus}
-          iconProps={{
-            id: 'Person',
-            width: 28,
-            height: 28,
-            color: 'gray04',
-          }}
-          className={`${
-            isFocused ? 'bg-[#E3E6E8]' : 'bg-[#F6F7F8]'
-          } w-[68px] h-[68px] flex justify-center items-center 
-    rounded-[100px] border-1 border-[#E3E6E8]`}
-        />
-      )}
-      {type === 'orange' && (
-        <IconButton
-          onClick={handleFocus}
-          iconProps={{
-            id: 'Person',
-            width: 28,
-            height: 28,
-            color: 'white',
-          }}
-          className={`${
-            isFocused ? 'bg-[#E85830]' : 'bg-[#FF744D]'
-          } w-[68px] h-[68px] flex justify-center items-center 
-    rounded-[100px] border-1 border-[#E3E6E8]`}
-        />
-      )}
+      <IconButton
+        onClick={handleFocus}
+        iconProps={iconProps}
+        className={commonIconButtonClasses}
+      />
     </div>
   );
 };
