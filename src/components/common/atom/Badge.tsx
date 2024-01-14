acab769a0620e501ae08395f9f383b0cadcefcd6
mj <minai621@naver.com>
2024-01-14 23:00:32 +0900

    refact: badge 리팩토링

--- src/components/common/atom/Badge.tsx
+++ src/components/common/atom/Badge.tsx
@@ -7,14 +7,13 @@ interface BadgeProps extends CodeProps {
 }
 
 const Badge = ({ type = 'default', children }: BadgeProps) => {
+  const badgeVariantClassed =
+    type === 'default'
+      ? 'text-gray07 bg-gray02'
+      : 'text-primary500 bg-primary100';
+
   return (
-    <Code
-      className={`${
-        type === 'default'
-          ? 'text-gray07 bg-gray02'
-          : 'text-primary500 bg-primary100'
-      }`}
-    >
+    <Code className={badgeVariantClassed}>
       <Typography variant="caption01">{children}</Typography>
     </Code>
   );
