fefc7ef1a3e857efb391eaacb88e439be6ea399e
mj <minai621@naver.com>
2024-01-16 13:27:08 +0900

    refac: BottomSheet 로직 수정

--- src/components/common/organism/BottomSheet.tsx
+++ src/components/common/organism/BottomSheet.tsx
@@ -1,13 +1,21 @@
 import BottomSheetListItem from '@components/common/molecule/BottomSheetListItem';
 import { Drawer, DrawerContent } from '@components/shadcn/drawer';
 import { bottomSheetState } from '@store/bottomSheet';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
 
 export default function BottomSheet() {
-  const bottomSheet = useRecoilValue(bottomSheetState);
+  const [bottomSheet, setBottomSheet] = useRecoilState(bottomSheetState);
 
   return (
-    <Drawer open={bottomSheet.isOpen}>
+    <Drawer
+      open={bottomSheet.isOpen}
+      onOpenChange={open =>
+        setBottomSheet(prev => ({
+          ...prev,
+          isOpen: open,
+        }))
+      }
+    >
       <DrawerContent>
         <div className="p-4">
           {bottomSheet.contents.map(({ title, subtitle, onClick }) => (
