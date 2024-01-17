14155f7da3411ff0df76efa152b9a48e5b0a293c
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2024-01-17 20:40:21 +0900

    chore: mkcert 재설치 및 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   resolve: {
     alias: [
       { find: '@constants', replacement: '/src/constants' },
