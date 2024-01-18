def581d7773a7b8365ad5259179f116a3af4147e
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2024-01-18 23:59:16 +0900

    feat: Redirect 페이지 라우팅 설정 및 상수화

--- src/constants/activities.ts
+++ src/constants/activities.ts
@@ -1,4 +1,5 @@
 import Home from '@components/home/template/Home';
+import Redirect from '@components/login/template/Redirect';
 import Mypage from '@components/mypage/template/Mypage';
 import Notification from '@components/notification/template/Notification';
 import Peer from '@components/peer/template/Peer';
@@ -6,6 +7,7 @@ import Project from '@components/project/template/Project';
 
 export const Activities = {
   Home,
+  Redirect,
   Peer,
   Project,
   Notification,
