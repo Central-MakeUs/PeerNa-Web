0d0b8b48ff10577bac707bc0eab587899d177921
sy-paik <sypaik.dev@gmail.com>
2024-01-25 21:50:25 +0900

    refactor: Typo number 타입 추가 및 너비 full 로 변경

--- src/components/common/atom/Typography.tsx
+++ src/components/common/atom/Typography.tsx
@@ -4,7 +4,7 @@ import { HTMLAttributes } from 'react';
 
 export interface TypographyProps extends HTMLAttributes<HTMLDivElement> {
   variant: FontVariantsKeys;
-  children: string;
+  children: string | number;
   fontColor?: ColorTypes;
   as?: keyof JSX.IntrinsicElements;
 }
