e15bc81cc856324a92d9efcd5e8e76b6d617e1f0
sy-paik <sypaik.dev@gmail.com>
2024-01-25 20:47:05 +0900

    refactor: icon props 추가 및 스타일 변경

--- src/components/common/atom/Chip.tsx
+++ src/components/common/atom/Chip.tsx
@@ -7,45 +7,56 @@ import {
 } from '@nextui-org/react';
 import { ColorTypes } from '@type/styles';
 import { getRgbaColorWithOpacity } from '@utils/styles';
+import { IconKeyType } from '@constants/icons';
 
 interface ChipProps extends ChipPropsWithNextui {
-  type: keyof typeof ChipTypeClassNames;
+  chipType: keyof typeof ChipTypeClassNames;
   children: string;
+  icon: string;
 }
 
 const ChipTypeClassNames: Record<
-  'C' | 'I' | 'D' | 'S' | 'isDisabled',
+  'D' | 'I' | 'S' | 'C' | 'isDisabled',
   Extract<
     ColorTypes,
     | 'secondary-purple'
     | 'secondary-pink'
-    | 'primary100'
+    | 'primary500'
     | 'secondary-yellow'
     | 'gray04'
   >
 > = {
-  C: 'secondary-purple',
-  I: 'secondary-pink',
-  D: 'primary100',
-  S: 'secondary-yellow',
+  C: 'secondary-yellow',
+  I: 'primary500',
+  D: 'secondary-purple',
+  S: 'secondary-pink',
   isDisabled: 'gray04',
 };
 
-export default function Chip({ type, children }: ChipProps) {
-  const chipColor = Palette[ChipTypeClassNames[type]];
-  const bgOpacityStyle = getRgbaColorWithOpacity(chipColor, 0.2);
-  const textColor: ColorTypes =
-    chipColor === Palette.primary100 ? 'primary500' : ChipTypeClassNames[type];
-  const textStyle =
-    type === 'isDisabled' ? { color: Palette.gray04 } : undefined;
+export default function Chip({ chipType, children, icon }: ChipProps) {
+  const chipColor = ChipTypeClassNames[chipType];
+  const bgOpacityStyle = getRgbaColorWithOpacity(Palette[chipColor], 0.2);
+  const bgStyle =
+    chipType === 'isDisabled'
+      ? { backgroundColor: Palette.gray01 }
+      : chipType === 'I'
+        ? { backgroundColor: Palette.primary100 }
+        : bgOpacityStyle;
+
+  const textStyle = chipType === 'isDisabled' ? 'gray04' : 'gray08';
 
   return (
-    <ChipWithNextui className="h-[36px]" style={bgOpacityStyle}>
+    <ChipWithNextui className="flex !px-2 h-[36px]" style={bgStyle}>
       <div className="flex flex-nowrap gap-1">
-        <Typography variant="body04" style={textStyle}>
+        <Typography variant="body04" fontColor={textStyle}>
           {children}
         </Typography>
-        <SvgIcon id="Process" width={20} height={20} color={textColor} />
+        <SvgIcon
+          id={icon as unknown as IconKeyType}
+          width={20}
+          height={20}
+          color={chipColor}
+        />
       </div>
     </ChipWithNextui>
   );
