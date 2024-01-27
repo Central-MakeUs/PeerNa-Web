d3035f9f0eefb8e15dbfed8ca0189204bbe92faa
sy-paik <sypaik.dev@gmail.com>
2024-01-27 18:43:51 +0900

    feat: 더보기 화살표 props 추가

--- src/components/pages/mypage/molecule/HeaderContainer.tsx
+++ src/components/pages/mypage/molecule/HeaderContainer.tsx
@@ -1,13 +1,15 @@
 interface HeaderContainerProps {
   size: 'md' | 'sm';
   children: React.ReactNode;
+  arrow?: boolean;
 }
 
 export default function HeaderContainer({
   size,
   children,
+  arrow,
 }: HeaderContainerProps) {
-  const containerClassName = `pl-5 ${size === 'sm' ? 'pt-8 pb-3' : 'pt-10 pb-3'}`;
+  const containerClassName = `pl-5 ${arrow ? 'flex justify-between' : ''} ${size === 'sm' ? 'pt-8 pb-3' : 'pt-10 pb-3'}`;
 
   return <section className={containerClassName}>{children}</section>;
 }
