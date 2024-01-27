d618b6c2e2a2670e31b7b2ac5d2fdc6efcc25ac8
sy-paik <sypaik.dev@gmail.com>
2024-01-27 19:09:59 +0900

    feat: 프로필 수정 리스트 추가

--- src/components/pages/profileEdit/organism/ProfileEditList.tsx
+++ src/components/pages/profileEdit/organism/ProfileEditList.tsx
@@ -2,31 +2,46 @@ import Dropdown from '@components/common/atom/Dropdown';
 import TextArea from '@components/common/atom/TextArea';
 import HeaderContainer from '@components/pages/profileEdit/molecule/HeaderContainer';
 import Typography from '@components/common/atom/Typography';
+import { ProfileInfo } from '@hooks/query/useProfileInfo';
+import { ProfileSelfStateType } from '@store/profileSelfState';
 
-const handleSelect = () => {
-  console.log('클릭');
-};
+interface ProfileEditProps {
+  profileSelf: ProfileSelfStateType;
+  myProfileInfo: ProfileInfo;
+  handleClickJob: () => void;
+  handleClickPart: () => void;
+  handleChangeOneLiner: (newLiner: string) => void;
+}
 
-export default function ProfileEditList({ values }: { values: string }) {
+export default function ProfileEditList({
+  profileSelf,
+  handleClickPart,
+  handleClickJob,
+  handleChangeOneLiner,
+}: ProfileEditProps) {
+  console.log(profileSelf.oneLiner);
   return (
     <ul>
       <li className="px-5">
         <HeaderContainer>
           <Typography variant="header03">직업</Typography>
         </HeaderContainer>
-        <Dropdown values={values}></Dropdown>
+        <Dropdown value={profileSelf.job} onClick={handleClickJob}></Dropdown>
       </li>
       <li className="px-5">
         <HeaderContainer>
-          <Typography variant="header03">직업</Typography>
+          <Typography variant="header03">직무</Typography>
         </HeaderContainer>
-        <Dropdown values={values}></Dropdown>
+        <Dropdown value={profileSelf.part} onClick={handleClickPart}></Dropdown>
       </li>
       <li className="px-5">
         <HeaderContainer>
           <Typography variant="header03">한 줄 소개</Typography>
         </HeaderContainer>
-        <TextArea text="한줄소개" handleChangeText={handleSelect}></TextArea>
+        <TextArea
+          text={profileSelf.oneLiner}
+          handleChangeText={handleChangeOneLiner}
+        ></TextArea>
       </li>
     </ul>
   );
