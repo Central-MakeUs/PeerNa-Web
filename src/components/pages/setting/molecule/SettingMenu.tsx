814fa1bc5c39c9674a1986c944da4dc18a418ae8
sy-paik <sypaik.dev@gmail.com>
2024-01-27 18:36:14 +0900

    feat: 설정 리스트 수정

--- src/components/pages/setting/molecule/SettingMenu.tsx
+++ src/components/pages/setting/molecule/SettingMenu.tsx
@@ -1,9 +1,13 @@
 import Typography from '@components/common/atom/Typography';
 
 export default function SettingMenu() {
+  const handleDelete = () => {
+    console.log('탈퇴');
+  };
+
   const containerStyle = 'pt-[33px] pl-[28px] pb-[17px]';
   return (
-    <ul>
+    <ul className="w-full">
       <li className={containerStyle}>
         <Typography variant="body01" fontColor="gray08">
           알림 허용
@@ -16,7 +20,17 @@ export default function SettingMenu() {
       </li>
       <li className={containerStyle}>
         <Typography variant="body01" fontColor="gray08">
-          탈퇴
+          개인정보처리방침
+        </Typography>
+      </li>
+      <li className={containerStyle}>
+        <Typography variant="body01" fontColor="gray08">
+          마케팅 정보 활용 동의약관
+        </Typography>
+      </li>
+      <li className={containerStyle} onClick={handleDelete}>
+        <Typography variant="body01" fontColor="gray08">
+          회원 탈퇴
         </Typography>
       </li>
     </ul>
--- src/components/pages/setting/template/SettingPage.tsx
+++ src/components/pages/setting/template/SettingPage.tsx
@@ -1,9 +1,27 @@
+import TopHeader from '@components/common/organism/TopHeader';
+import AppScreenContainer from '@components/wrapper/AppScreenContainter';
 import SettingMenu from '../molecule/SettingMenu';
+import { useFlow } from '@hooks/useStackFlow';
+import FixedButtonContainer from '@components/wrapper/FixedButtonContainer';
+import FixedBottomButton from '@components/wrapper/FixedBottomButton';
 
 export default function SettingPage() {
+  const { pop } = useFlow();
+  const handleClick = () => pop();
+  const handleLogout = () => {
+    console.log('로그아웃');
+  };
   return (
     <>
-      <SettingMenu />
+      <AppScreenContainer>
+        <TopHeader title="설정" onClick={handleClick} />
+        <SettingMenu />
+      </AppScreenContainer>
+      <FixedButtonContainer direction="row">
+        <FixedBottomButton buttonVariant="naked" handleClick={handleLogout}>
+          로그아웃
+        </FixedBottomButton>
+      </FixedButtonContainer>
     </>
   );
 }
