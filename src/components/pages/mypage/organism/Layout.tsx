976186a7af89fbaf609f9100dc83bc32a0b66aeb
sy-paik <sypaik.dev@gmail.com>
2024-01-27 19:14:11 +0900

    style: height 임의 수정

--- src/components/pages/mypage/organism/Layout.tsx
+++ src/components/pages/mypage/organism/Layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="w-full flex-1 bg-white h-[2000px] overflow-y-auto rounded-tl-3xl rounded-tr-3xl">
+    <div className="w-full flex-1 h-[20000px] bg-white overflow-y-auto rounded-tl-3xl rounded-tr-3xl">
       {children}
     </div>
   );
