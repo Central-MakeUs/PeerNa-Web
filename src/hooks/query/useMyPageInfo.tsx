21b7e76fa4f1295e7a943207b25f508a1a857ae2
sy-paik <sypaik.dev@gmail.com>
2024-01-27 18:50:45 +0900

    feat: 마이페이지 정보 조회 쿼리 추가

--- src/hooks/query/useMyPageInfo.tsx
+++ src/hooks/query/useMyPageInfo.tsx
@@ -1,44 +1,11 @@
-import { AxiosResponse } from 'axios';
 import { getMypageInfo } from '@apis/mypage';
 import { useQuery } from '@tanstack/react-query';
-import { ProfileCardInfo } from '@components/pages/mypage/molecule/ProfileCard';
-import { CardType } from '@constants/image';
-import { OverallOpinionProps } from '@components/pages/mypage/molecule/OverallOpinion';
-
-interface MyPageResponse {
-  code: number;
-  message: string;
-  result: MyPageInfo;
-}
-
-interface MyPageInfo {
-  peerTestMoreThanTree: boolean;
-  memberSimpleInfoDto: ProfileCardInfo;
-  peerTestType: string;
-  selfTestCardList: CardType[];
-  peerCardList: CardType[];
-  totalEvaluation: OverallOpinionProps;
-  totalScore: number;
-  peerFeedbackList: string[];
-  selfTestAnswerIdList: number[];
-  colorAnswerIdList: number[];
-}
 
 export const useMyPageInfo = () => {
-  const { data, error, isLoading, isError } = useQuery<MyPageInfo, Error>({
+  const { data, error, isLoading, isError } = useQuery({
     queryKey: ['mypageInfo'],
-    queryFn: async () => {
-      const response: AxiosResponse<MyPageResponse> = await getMypageInfo();
-
-      if (!response.data) {
-        throw new Error('오류발생!');
-      }
-
-      const responseData: MyPageInfo = response.data.result;
-      return responseData;
-    },
-    staleTime: 60 * 60 * 1000,
+    queryFn: getMypageInfo,
   });
 
-  return { data, error, isLoading, isError };
+  return { data: data?.result, error, isLoading, isError };
 };
