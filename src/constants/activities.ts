c4f2f64aed4a8837ee879da2eee46a23243051db
sy-paik <sypaik.dev@gmail.com>
2024-01-29 03:51:55 +0900

    feat: 각 피어 유형별 페이지 추가 (#62)

--- src/constants/activities.ts
+++ src/constants/activities.ts
@@ -2,6 +2,10 @@ import HomePage from '@components/pages/home/template/HomePage';
 import AppleRedirectPage from '@components/pages/login/template/AppleRedirectPage';
 import KakaoRedirectPage from '@components/pages/login/template/KakaoRedirectPage';
 import MyPage from '@components/pages/mypage/template/MyPage';
+import PeerTypeDPage from '@components/pages/peerType/template/PeerTypeDPage';
+import PeerTypeIPage from '@components/pages/peerType/template/PeerTypeIPage';
+import PeerTypeSPage from '@components/pages/peerType/template/PeerTypeSPage';
+import PeerTypeCPage from '@components/pages/peerType/template/PeerTypeCPage';
 import SettingPage from '@components/pages/setting/template/SettingPage';
 import MoreFeedbackPage from '@components/pages/moreFeedback/template/MoreFeedbackPage';
 import NotificationPage from '@components/pages/notification/template/NotificationPage';
@@ -16,6 +20,10 @@ import ReviewSelfPage from '@components/pages/reviewSelf/template/ReviewSelfPage
 
 export const Pages = {
   HomePage,
+  PeerTypeDPage,
+  PeerTypeIPage,
+  PeerTypeSPage,
+  PeerTypeCPage,
   PeerPage,
   ProjectPage,
   NotificationPage,
@@ -34,6 +42,10 @@ export const Pages = {
 
 export const PageRoutes: Record<keyof typeof Pages, string> = {
   HomePage: '/',
+  PeerTypeDPage: '/peer/type/D',
+  PeerTypeIPage: '/peer/type/I',
+  PeerTypeSPage: '/peer/type/S',
+  PeerTypeCPage: '/peer/type/C',
   PeerPage: '/peer',
   ProjectPage: '/project',
   NotificationPage: '/notification',
