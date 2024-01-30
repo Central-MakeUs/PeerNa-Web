a2b660659312230fdc186da45800fe738dbd0497
mj <minai621@naver.com>
2024-01-30 22:23:26 +0900

    fix: AlarmListItem 잘못된 태그 닫힘 수정

--- src/components/common/molecule/AlarmListItem.tsx
+++ src/components/common/molecule/AlarmListItem.tsx
@@ -1,5 +1,4 @@
 import IconButton from '@components/common/atom/IconButton';
-import SvgIcon from '@components/common/atom/SvgIcon';
 import Typography from '@components/common/atom/Typography';
 import { Palette } from '@constants/styles';
 import { useFlow } from '@hooks/useStackFlow';
@@ -14,26 +13,28 @@ export default function AlarmListItem({ title, subtitle }: AlarmListItemProps) {
   };
 
   return (
-      <div className="flex justify-between items-center w-full px-5 py-4 rounded-xl bg-primary100 cursor-pointer">
+    <div className="flex justify-between items-center w-full px-5 py-4 rounded-xl bg-primary100 cursor-pointer">
       <div className="flex gap-3">
         <div
-          className={`w-[24px] h-[24px] p-3 box-content rounded-full bg-primary200`} />
+          className={`w-[24px] h-[24px] p-3 box-content rounded-full bg-primary200`}
+        />
+        <div className="flex flex-col gap-1">
+          <Typography variant="body01">{title}</Typography>
+          <Typography
+            variant="body05"
+            className={`text-[${Palette.gray05}] text-left`}
+          >
+            {subtitle}
+          </Typography>
+        </div>
       </div>
-      <div className="flex flex-col gap-1">
-        <Typography variant="body01">{title}</Typography>
-        <Typography
-          variant="body05"
-          className={`text-[${Palette.gray05}] text-left`}
-        >
-          {subtitle}
-        </Typography>
-      </div>
-    </div><IconButton
+      <IconButton
         onClick={handleClick}
         iconProps={{
           id: 'IOSChevronRight',
           color: 'gray07',
-        }} />
+        }}
+      />
     </div>
   );
 }
