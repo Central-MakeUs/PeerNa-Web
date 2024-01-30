fd8364488b95c078b12ab6e15fb21a73eef0684b
mj <minai621@naver.com>
2024-01-30 22:06:04 +0900

    feat: apple 로그인 정보 environtment 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { http } from '@apis/index';
 import BottomSheet from '@components/common/organism/BottomSheet';
 import LoginModal from '@components/pages/login/organism/LoginModal';
 import { Stack } from '@hooks/useStackFlow';
@@ -9,11 +10,24 @@ function App() {
   useEffect(() => {
     WebviewBridge.registerMessageListener();
     window.AppleID.auth.init({
-      clientId: 'com.peerna.service',
+      clientId: import.meta.env.VITE_APPLE_CLIENT_ID,
       scope: 'email',
-      redirectURI: 'https://dev.peerna.me/member/login/oauth2/apple',
+      redirectURI: import.meta.env.VITE_APPLE_REDIRECT_URI,
       usePopup: false,
     });
+
+    http
+      .post('/test/fcm', {
+        fcmToken:
+          'eRj6MwMtVUzXgKZXuXug11:APA91bGxJLaJFoJgj404jryExJNkcv7QGx-tni3T7PBdNdUOx-5wYAxNc0xQ6lLPzyrKZ4BxUspeRIxwSgX_hxV4iYE-ZJnC1HKSXPtw8mGcFmK5RAbzv-zwAC0Njm3jyenf9u7xOzXi',
+      })
+      .then(() => {
+        console.log('success');
+        alert('success');
+      })
+      .catch(error => {
+        alert('error' + error.message);
+      });
   }, []);
 
   return (
