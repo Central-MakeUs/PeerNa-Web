644235b5650f77deb6b699a2b6d28001e1632830
sy-paik <sypaik.dev@gmail.com>
2024-02-01 03:38:01 +0900

    feat: Observer 훅 작성

---
+++ src/hooks/useIntersection.tsx
@@ -0,0 +1,33 @@
+import { useCallback, useEffect, useRef } from 'react';
+
+const useIntersection = (
+  onIntersect: (
+    entry: IntersectionObserverEntry,
+    observer: IntersectionObserver,
+  ) => void,
+  options?: IntersectionObserverInit,
+) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const callback = useCallback(
+    (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) onIntersect(entry, observer);
+      });
+    },
+    [onIntersect],
+  );
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(ref.current);
+
+    return () => observer.disconnect();
+  }, [ref, options, callback]);
+
+  return ref;
+};
+
+export default useIntersection;
