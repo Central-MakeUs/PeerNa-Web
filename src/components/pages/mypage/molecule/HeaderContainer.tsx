3ed66631b4cd1932ee41f0ebdb8f8350df7325a1
sy-paik <sypaik.dev@gmail.com>
2024-02-01 03:22:49 +0900

    style: className props 추가

--- src/components/pages/mypage/molecule/HeaderContainer.tsx
+++ src/components/pages/mypage/molecule/HeaderContainer.tsx
@@ -2,14 +2,16 @@ interface HeaderContainerProps {
   size: 'md' | 'sm';
   children: React.ReactNode;
   arrow?: boolean;
+  className?: string;
 }
 
 export default function HeaderContainer({
   size,
   children,
   arrow,
+  className,
 }: HeaderContainerProps) {
-  const containerClassName = `pl-5 ${arrow ? 'flex justify-between' : ''} ${size === 'sm' ? 'pt-8 pb-3' : 'pt-10 pb-3'}`;
+  const containerClassName = `pl-5 ${arrow ? 'flex justify-between' : ''} ${size === 'sm' ? 'pt-8 pb-3' : 'pt-10 pb-8'} ${className}`;
 
   return <section className={containerClassName}>{children}</section>;
 }
