c39140f8e92101d5e346a95243d2f78dd3255477
sy-paik <sypaik.dev@gmail.com>
2024-02-02 17:25:05 +0900

    fix: 홈페이지와 동료 페이지 쿼리 타입 전체 수정

--- src/hooks/api/useGetSearchPeerPart.tsx
+++ src/hooks/api/useGetSearchPeerPart.tsx
@@ -1,18 +1,13 @@
 import { http, ApiResponse } from '@apis/index';
 import { useInfiniteQuery } from '@tanstack/react-query';
-import { AxiosError } from 'axios';
 
-interface MemberSimpleDTOPage {
+export interface MemberSimpleDTOPage {
   memberSimpleProfileDtoList: MemberSimpleProfileDTO[];
   totalElements: number;
   currentPageElements: number;
   totalPage: number;
   isFirst: boolean;
   isLast: boolean;
-}
-
-interface PageResponse<T> extends ApiResponse<T> {
-  currentPage: number;
 }
 
 export interface MemberSimpleProfileDTO {
@@ -27,25 +22,22 @@ export interface MemberSimpleProfileDTO {
 
 export const getSearchPeerPart = async (
   peerPart: string,
-  pageParam: number,
-): Promise<PageResponse<MemberSimpleDTOPage>> => {
+  page: number,
+): Promise<ApiResponse<MemberSimpleDTOPage>> => {
   const response = await http.get(
-    `/home/search/peer-part?part=${peerPart}&page=${pageParam}`,
+    `/home/search/peer-part?part=${peerPart}&page=${page}`,
   );
-  return { ...response.data, pageParam };
+  return response.data;
 };
 
 export const useGetSearchPeerPart = (peerPart: string) =>
-  useInfiniteQuery<PageResponse<MemberSimpleDTOPage>, AxiosError>({
+  useInfiniteQuery<ApiResponse<MemberSimpleDTOPage>>({
     queryKey: ['getSearchPeerPart', peerPart],
-    queryFn: ({ pageParam = 1 }) => getSearchPeerPart(peerPart, pageParam),
-    getNextPageParam: lastPage => {
-      const nextPage =
-        lastPage?.result?.isLast === false ? lastPage.pageParam + 1 : undefined;
-      return nextPage;
+    queryFn: ({ pageParam = 1 }) =>
+      getSearchPeerPart(peerPart, pageParam as number),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) => {
+      const nextPage = allPages.length + 1;
+      return lastPage?.result?.isLast ? undefined : nextPage;
     },
-    select: data =>
-      (data?.pages ?? []).flatMap(
-        part => part?.result?.memberSimpleProfileDtoList ?? [],
-      ),
   });
