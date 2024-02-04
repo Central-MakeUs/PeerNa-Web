19f1ba4b0ac316de06e9af663d9267a274a4cd96
sy-paik <sypaik.dev@gmail.com>
2024-02-04 18:50:01 +0900

    refactor: 이미지 상수 추가

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,14 +1,56 @@
 import overThreeCheckReview from '@assets/OverThreeCheckReview.png';
 import kakaoShare from '@assets/kakao-talk.png';
-import peer_bg from '@assets/peerBg.png';
-import lottie1 from '@assets/review/01성향.json';
-import lottie2 from '@assets/review/02사고방식.json';
-import lottie3 from '@assets/review/03소통방식.json';
-import lottie4 from '@assets/review/04결정방식.json';
+import Onboarding1 from '@assets/onboarding/Onboarding1.png';
+import Onboarding2 from '@assets/onboarding/Onboarding2.png';
+import Onboarding3 from '@assets/onboarding/Onboarding3.png';
+import Onboarding4 from '@assets/onboarding/Onboarding4.png';
+import lottie1 from '@assets/review/01성향.json';
+import lottie2 from '@assets/review/02사고방식.json';
+import lottie3 from '@assets/review/03소통방식.json';
+import lottie4 from '@assets/review/04결정방식.json';
+
+import Peer1 from '@assets/review/peer/peer1.png';
+import Peer2 from '@assets/review/peer/peer2.png';
+import Peer3 from '@assets/review/peer/peer3.png';
+
+import Analytical from '@assets/common/analytical.png';
+import CardLocked from '@assets/common/card_locked.png';
+import Cautious from '@assets/common/cautious.png';
+import Challenging from '@assets/common/challenging.png';
+import Comprehensive from '@assets/common/comprehesive.png';
+import Cooperative from '@assets/common/cooperative.png';
+import Driving from '@assets/common/driving.png';
+import FutureOriented from '@assets/common/future_oriented.png';
+import Multidimensional from '@assets/common/multidimensional.png';
+import Pragmatic from '@assets/common/pragmatic.png';
+import Warmhearted from '@assets/common/warmhearted.png';
+
 
 export const REVIEW_LOTTIES = [lottie1, lottie2, lottie3, lottie4];
 
-export const KakaoShareImage = kakaoShare;
+export const KAKAO_SHARE = kakaoShare;
 
-export const OverThreeCheckReview = overThreeCheckReview;
-export const peerBg = peer_bg;
+export const OVER_THREE_CHECK_REVIEW = overThreeCheckReview;
+
+export const ON_BOARDING_IMAGE_LIST = [
+  Onboarding1,
+  Onboarding2,
+  Onboarding3,
+  Onboarding4,
+];
+
+export const PEER_REVIEW_IMAGE_LIST = [Peer1, Peer2, Peer3];
+
+export const KEYWORD_IMAGE_MAPPER = {
+  ANALYTICAL: Analytical,
+  CAUTIOUS: Cautious,
+  CHALLENGING: Challenging,
+  COMPREHENSIVE: Comprehensive,
+  COOPERATIVE: Cooperative,
+  DRIVING: Driving,
+  FUTURE_ORIENTED: FutureOriented,
+  MULTI_DIMENSIONAL: Multidimensional,
+  PRAGMATIC: Pragmatic,
+  WARMHEARTED: Warmhearted,
+  CARD_LOCKED: CardLocked
+}
