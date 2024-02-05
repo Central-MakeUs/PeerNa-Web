7b05faf0aa9f74547455b3bfe4ffc767b6a4b383
sy-paik <sypaik.dev@gmail.com>
2024-02-05 23:49:16 +0900

    rename: PeerCard 컴포넌트 구현

--- src/components/common/atom/PeerCard.tsx
+++ src/components/common/atom/PeerCard.tsx
@@ -1,18 +1,20 @@
 import Badge from '@components/common/atom/Badge';
-import { TEST_TYPE_INFO } from "@constants/list";
+import { TEST_TYPE_INFO } from '@constants/list';
+import { ICON_MAPPER } from '@constants/mapper';
 import Typography from './Typography';
 
 interface CardProps {
   size: 'M' | 'L';
-  type: 'peer' | 'self';
+  type?: 'peer' | 'self';
   testResult: 'D' | 'I' | 'S' | 'C';
 }
 
-export default function Card({ size, type, testResult }: CardProps) {
+export default function PeerCard({ size, type, testResult }: CardProps) {
   const cardType = type === 'peer' ? 'primary' : 'default';
   const badgeText = type === 'peer' ? '피어테스트' : '셀프 테스트';
+  const peerBg = ICON_MAPPER[testResult];
 
-  const bgCard = `bg-C bg-[center_top_3rem] bg-no-repeat`;
+  const bgCard = `${peerBg} bg-[center_top_3rem] bg-no-repeat`;
 
   const cardSize =
     size === 'L'
@@ -21,25 +23,27 @@ export default function Card({ size, type, testResult }: CardProps) {
 
   return (
     <div
-      className={`${bgCard} ${cardSize} rounded-2xl border-1 border-[#E3E6E8] flex flex-col items-center justify-between`}
+      className={`${bgCard} ${cardSize} bg-white rounded-2xl border-1 border-[#E3E6E8] flex flex-col items-center justify-between mb-3`}
     >
-      <Badge type={cardType} className="mb-1.5">
-        {badgeText}
-      </Badge>
-      <p>
+      {type && (
+        <Badge type={cardType} className="mb-1.5">
+          {badgeText}
+        </Badge>
+      )}
+      <p className="mt-auto">
         <Typography
           variant="header03"
           fontColor="gray08"
           className="text-center"
         >
-          {TEST_TYPE_INFO[testResult].title}
+          {TEST_TYPE_INFO[testResult]?.title}
         </Typography>
         <Typography
           variant="body03"
           fontColor="gray04"
           className={`text-center ${size === 'L' ? '!whitespace-nowrap mt-2' : ''}`}
         >
-          {TEST_TYPE_INFO[testResult].description}
+          {TEST_TYPE_INFO[testResult]?.description}
         </Typography>
       </p>
     </div>
