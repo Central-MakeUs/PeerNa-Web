e443e217ca660e91cae48daa74b955d55416b1e3
sy-paik <sypaik.dev@gmail.com>
2024-02-06 17:21:41 +0900

    feat: 카카오 공유 핸들러 훅 작성

--- src/pages/home/index/atom/ReviewButton.tsx
+++ src/pages/home/index/atom/ReviewButton.tsx
@@ -1,36 +1,17 @@
 import SvgIcon from '@components/common/atom/SvgIcon';
 import Typography from '@components/common/atom/Typography';
+import useSendKakaoMessage from '@hooks/common/useSendKakoMessage';
 
 export default function ReviewButton() {
-  const handleSendKakaoMessage = () => {
-    window.Kakao.Link.sendDefault({
-      objectType: 'feed',
-      // 해당 content들은 모두 변경 예정
-      content: {
-        title: '평판 요청합니다',
-        description: '평판을 작성해주세요!',
-        imageUrl: 'https://localhost:5173/',
-        link: {
-          webUrl: 'https://localhost:5173/',
-          mobileWebUrl: 'https://localhost:5173/',
-        },
-      },
-      buttons: [
-        {
-          title: '평판 작성하기',
-          link: {
-            webUrl: 'https://localhost:5173/',
-            mobileWebUrl: 'https://localhost:5173/',
-          },
-        },
-      ],
-    });
-  };
+  const handleSendKakaoMessage = useSendKakaoMessage();
+  const title = '평판 요청';
+  const description = '평판을 작성하세요!';
+
   return (
     <div className="mt-8 mb-10 max-w-[524px] mx-auto">
       <button
         className="bg-primary100 rounded-xl w-full flex px-4 py-5 justify-between items-center"
-        onClick={handleSendKakaoMessage}
+        onClick={() => handleSendKakaoMessage({ title, description })}
       >
         <div>
           <Typography variant="header03" fontColor="gray08" className="mb-1">
