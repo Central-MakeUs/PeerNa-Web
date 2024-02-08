fb309d4142fcaf9bbc0a76c4d8f4770df60bce31
sy-paik <sypaik.dev@gmail.com>
2024-02-09 06:59:22 +0900

    feat: 로그아웃과 회원탈퇴 구현

--- src/pages/mypage/setting/template/SettingPage.tsx
+++ src/pages/mypage/setting/template/SettingPage.tsx
@@ -2,16 +2,37 @@ import Button from '@components/common/atom/Button';
 import AppScreenContainer from '@components/wrapper/AppScreenContainter';
 import Footer from '@components/wrapper/Footer';
 import Header from '@components/wrapper/Header';
+import usePostLogout from '@hooks/api/member/index/usePostLogout';
+import usePostMemberWithdrawal from '@hooks/api/member/index/usePostMemberWithdrawal';
 import { useFlow } from '@hooks/common/useStackFlow';
 import { ActivityComponentType } from '@stackflow/react';
+import { getFcmToken, getRefreshToken } from '@utils/token';
+import toast from 'react-hot-toast';
 import SettingMenu from '../molecule/SettingMenu';
 
 const SettingPage: ActivityComponentType = () => {
+  const { replace } = useFlow();
+  const refreshToken = getRefreshToken();
+  const fcmToken = getFcmToken() ?? '';
   const { pop } = useFlow();
   const handleClick = () => pop();
+  const { mutate: logoutMutate } = usePostLogout();
+  const { mutate: withdrawalMutate } = usePostMemberWithdrawal();
+
   const handleLogout = () => {
-    console.log('로그아웃');
+    if (refreshToken)
+      logoutMutate({ refreshToken: refreshToken, fcmToken: fcmToken });
+    toast.success('로그아웃 되었습니다');
+    localStorage.clear();
+    replace('OnboardingPage', { step: '1' });
   };
+  const handleDelete = () => {
+    withdrawalMutate();
+    toast.success('회원탈퇴 되었습니다');
+    localStorage.clear();
+    replace('OnboardingPage', { step: '1' });
+  };
+
   return (
     <AppScreenContainer>
       <Header>
@@ -20,7 +41,7 @@ const SettingPage: ActivityComponentType = () => {
           <Header.Title className="mx-auto">설정</Header.Title>
         </Header.TopBar>
       </Header>
-      <SettingMenu />
+      <SettingMenu handleDelete={handleDelete} />
       <Footer bottom={3}>
         <Button buttonVariant="naked" onClick={handleLogout}>
           로그아웃
