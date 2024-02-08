ba31fecb7433769d30b8308c932b8923909ee881
sy-paik <sypaik.dev@gmail.com>
2024-02-09 01:56:08 +0900

    chore: 개행 nowrap 추가 및 너비값 삭제

--- src/components/common/atom/PeerTalk.tsx
+++ src/components/common/atom/PeerTalk.tsx
@@ -18,7 +18,7 @@ export default function PeerTalk({
 }: TalkProps) {
   const talkStyle = 'rounded-xl pl-4 pr-2 py-2 box-content';
   return (
-  <div className="flex w-full items-center">
+    <div className="flex w-full items-center">
       <Typography
         variant="body01"
         fontColor="primary"
@@ -33,10 +33,11 @@ export default function PeerTalk({
         className={talkStyle}
         classNames={{
           base: `bg-primary100`,
-          content: 'w-[316px]',
         }}
       >
-        <Typography variant="body02">{children}</Typography>
+        <Typography variant="body02" className="!whitespace-nowrap">
+          {children}
+        </Typography>
       </ChipWithNextui>
     </div>
   );
