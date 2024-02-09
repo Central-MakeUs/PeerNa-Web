4ba6cff225da31e8cb6a672a9074b1782e27d024
sy-paik <sypaik.dev@gmail.com>
2024-02-10 06:39:22 +0900

    style: 너비 및 높이 수정

--- src/components/common/atom/AvatarButton.tsx
+++ src/components/common/atom/AvatarButton.tsx
@@ -15,9 +15,9 @@ export default function AvatarButton({ type, handleClick }: AvatarButtonProps) {
   const bgImage = ICON_MAPPER[type];
   //TODO) 반응형으로 구현할 것
   return (
-    <div className="flex justify-center items-center shadow-default tablet:w-[105.88px] tablet:h-[105.88px] mobile:w-[72px] mobile:h-[72px] rounded-full">
+    <div className="flex justify-center items-center shadow-default w-[72px] h-[72px] rounded-full">
       <button
-        className={`${backgroundColor} ${bgImage} tablet:w-[100px] tablet:h-[100px] mobile:w-[68px] mobile:h-[68px] tablet:bg-68 mobile:bg-44 bg-no-repeat bg-center 
+        className={`${backgroundColor} ${bgImage} w-[68px] h-[68px] bg-44 bg-no-repeat bg-center 
   rounded-full border-1 border-[#E3E6E8]`}
         onClick={() => {
           handleFocus();
