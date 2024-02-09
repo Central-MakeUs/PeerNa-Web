4e810ae84bfed01559d6220fdc6e8b27a9dafa15
sy-paik <sypaik.dev@gmail.com>
2024-02-10 06:39:52 +0900

    feat: 피어테스트 요청 카카오톡 공유하기 추가

--- src/pages/mypage/index/template/MyPage.tsx
+++ src/pages/mypage/index/template/MyPage.tsx
@@ -8,9 +8,11 @@ import Content from '@components/wrapper/Content';
 import ErrorBoundaryWithSuspense from '@components/wrapper/ErrorBoundaryWithSuspense';
 import Footer from '@components/wrapper/Footer';
 import Header from '@components/wrapper/Header';
+import { REVIEW_REQUEST } from '@constants/images';
 import useGetMyPageInfo from '@hooks/api/member/index/useGetMypageInfo';
+import useSendKakaoMessage from '@hooks/common/useSendKakoMessage';
 import { useFlow } from '@hooks/common/useStackFlow';
-import { Tab } from '@nextui-org/react';
+import { Spacer, Tab } from '@nextui-org/react';
 import { ActivityComponentType } from '@stackflow/react';
 import SaveImageButton from '../atom/SaveImageButton';
 import CardTestResult from '../molecule/CardTestResult';
@@ -43,6 +45,12 @@ const MyPage: ActivityComponentType = () => {
   } = data;
 
   console.log(!!totalScore);
+
+  const handleSendKakaoMessage = useSendKakaoMessage();
+  const title = '저는 어떤 동료인가요?';
+  const description = '함께한 동료에 대해 알려주세요.';
+  const buttonText = '피어 테스트 응답하기';
+  const imagePath = REVIEW_REQUEST;
 
   const { push } = useFlow();
   const selfTestType = memberMyPageInfoDto.testType;
@@ -148,10 +156,23 @@ const MyPage: ActivityComponentType = () => {
                 )}
               </Tab>
             </RadioTabs>
-            <Button buttonVariant="primary" className="my-[40px]">
-              동료에게 물어보기
-            </Button>
-            <SaveImageButton />
+            <section className="flex flex-col gap-7 p-4">
+              <Button
+                buttonVariant="primary"
+                onClick={() =>
+                  handleSendKakaoMessage({
+                    title,
+                    description,
+                    buttonText,
+                    imagePath,
+                  })
+                }
+              >
+                동료에게 물어보기
+              </Button>
+              <SaveImageButton />
+            </section>
+            <Spacer y={20} />
           </Layout>
         </Content>
         <Footer>
