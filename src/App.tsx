cb882c7f2a4b3508aeccf789857c49fde18adeba
mj <minai621@naver.com>
2024-02-09 15:31:55 +0900

    fix: App에 기본 모달 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import ErrorFallback from '@components/common/molecule/ErrorFallback';
+import { Modals } from '@components/common/molecule/Modals';
 import BottomSheet from '@components/common/organism/BottomSheet';
 import { Stack } from '@hooks/common/useStackFlow';
 import { WebviewBridge } from '@utils/webview';
@@ -27,6 +28,7 @@ function App() {
         )}
       >
         <Stack />
+        <Modals />
         <BottomSheet />
         <Toaster
           toastOptions={{
