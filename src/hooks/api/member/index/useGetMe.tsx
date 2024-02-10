4829a3f4906f6f5fb56906de0c541383e67fab7a
sy-paik <sypaik.dev@gmail.com>
2024-02-11 05:12:04 +0900

    feat: enabled 옵션 사용

--- src/hooks/api/member/index/useGetMe.tsx
+++ src/hooks/api/member/index/useGetMe.tsx
@@ -1,7 +1,6 @@
 import { UtilityKeys } from '@constants/localStorage';
 import { QUERY_KEY } from '@constants/queryKey';
-import { useSuspenseQuery } from '@tanstack/react-query';
-import { JobType, PartType } from '@type/enums';
+import { useQuery } from '@tanstack/react-query';
 import { MemberDefaultInformationTypeWithUuid } from '@type/index';
 import { ApiResponse, http } from '@utils/API';
 
@@ -9,27 +8,17 @@ export interface MemberMeResponseDTO
   extends MemberDefaultInformationTypeWithUuid {}
 
 const getMe = async (): Promise<ApiResponse<MemberMeResponseDTO>> => {
-  const isOnboarding = localStorage.getItem(UtilityKeys.IS_ONBOARD);
-  if (!isOnboarding)
-    return {
-      code: 0,
-      message: '',
-      result: {
-        name: '',
-        uuid: '',
-        job: JobType.ENTREPRENEUR,
-        part: PartType.BACK_END,
-        oneLiner: '',
-      },
-    };
   const response = await http.get('/member/me');
   return response.data;
 };
 
 export default function useGetMe() {
-  return useSuspenseQuery({
+  const isOnboarding = localStorage.getItem(UtilityKeys.IS_ONBOARD);
+
+  return useQuery({
     queryKey: [QUERY_KEY.MEMBER_ME],
     queryFn: getMe,
     select: data => data.result,
+    enabled: !isOnboarding,
   });
 }
