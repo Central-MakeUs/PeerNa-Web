d62ffd539fc07df4242349d172b7c6ffc98b57be
sy-paik <sypaik.dev@gmail.com>
2024-02-10 20:52:59 +0900

    refactor: 푸시 알림 컴포넌트 사용

--- src/pages/mypage/setting/molecule/SettingMenu.tsx
+++ src/pages/mypage/setting/molecule/SettingMenu.tsx
@@ -1,10 +1,5 @@
-import Button from '@components/common/atom/Button';
 import Typography from '@components/common/atom/Typography';
-import ButtonContainer from '@components/common/molecule/ButtonContainer';
-import Modal from '@components/common/molecule/LegacyModal';
-import usePostPushAgree from '@hooks/api/member/index/usePostPushAgree';
 import useModal from '@hooks/store/useModal';
-import toast from 'react-hot-toast';
 
 interface SettingProps {
   handleDelete: () => void;
@@ -21,27 +16,13 @@ export default function SettingMenu({ handleDelete }: SettingProps) {
     window.location.href = url;
   };
 
-  const { openModal, closeModal } = useModal('alarm');
+  const { openModal } = useModal('push');
 
   const handlePushAlarm = () => {
     openModal();
   };
 
-  const { mutate } = usePostPushAgree();
-
-  const handleClickAlaramDecline = () => {
-    mutate({ pushAgree: false });
-    toast.success('푸시 알림 설정을 거부했습니다.');
-    closeModal();
-  };
-
-  const handleClickAlarmAccept = () => {
-    mutate({ pushAgree: true });
-    toast.success('푸시 알림 설정이 완료되었습니다');
-    closeModal();
-  };
-
-  const containerStyle = 'pt-[33px] pl-[28px] pb-[17px]';
+  const containerStyle = 'pt-[33px] pl-[28px] pb-[17px] cursor-pointer';
   return (
     <ul className="w-full">
       <li className={containerStyle} onClick={handlePushAlarm}>
@@ -70,19 +51,6 @@ export default function SettingMenu({ handleDelete }: SettingProps) {
           회원 탈퇴
         </Typography>
       </li>
-      <Modal
-        type="alarm"
-        modalHeader={`PeerNa에서 \n 알림을 보내고자 합니다`}
-        modalBody={`해당 기기로 피어 테스트 응답 요청 및 \n 프로젝트 제안 등 서비스 이용에 필요한 \n 안내 사항을 푸시 알림으로 알려드릴게요 \n \n 앱 푸시에 수신 동의하시겠습니까?`}
-        footer={
-          <ButtonContainer direction="row">
-            <Button buttonVariant="tertiary" onClick={handleClickAlaramDecline}>
-              허용 안함
-            </Button>
-            <Button onClick={handleClickAlarmAccept}>허용</Button>
-          </ButtonContainer>
-        }
-      />
     </ul>
   );
 }
