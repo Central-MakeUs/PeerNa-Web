fa57f48523cf8c383a8f0c61c7524b8d1f6cab70
sy-paik <sypaik.dev@gmail.com>
2024-02-10 21:49:51 +0900

    feat: 에러 모달 타입 추가

--- src/components/common/molecule/ErrorModal.tsx
+++ src/components/common/molecule/ErrorModal.tsx
@@ -1,67 +1,48 @@
+import Button from '@components/common/atom/Button';
 import Typography from '@components/common/atom/Typography';
+import useModal from '@hooks/store/useModal';
 import {
   ModalContent,
   ModalFooter,
-  ModalProps as ModalPropsWithNextui,
   Modal as ModalWithNextui,
 } from '@nextui-org/react';
-import { modalState } from '@store/modal';
-import { useRecoilState } from 'recoil';
 import SvgIcon from '../atom/SvgIcon';
 
-interface ErrorModalProps extends ModalPropsWithNextui {
-  modalHeader: string;
-  modalBody: string;
-  footer: React.ReactNode;
-}
+export default function ErrorModal() {
+  const { isOpen, openModal, closeModal } = useModal('error');
 
-export default function ErrorModal({
-  modalHeader,
-  modalBody,
-  footer,
-  ...props
-}: ErrorModalProps) {
-  const [modal, setModal] = useRecoilState(modalState);
-
-  const handleSetIsOpen = (isOpen: boolean) => {
-    setModal({
-      ...modal,
-      isOpen: !isOpen,
-      modalType: 'error',
-    });
+  const handleOpenChange = (isOpen: boolean) => {
+    if (isOpen) openModal();
+    else closeModal();
   };
 
   return (
     <>
-      {modal.isOpen && (
-        <ModalWithNextui
-          {...props}
-          backdrop="opaque"
-          isOpen={modal.isOpen}
-          onOpenChange={handleSetIsOpen}
-          hideCloseButton={true}
-          classNames={{
-            backdrop:
-              'bg-gradient-to-t from-zinc-900 to-zinc-900/10 backdrop-opacity-20',
-          }}
-        >
-          <ModalContent className="w-[310px] m-auto">
-            <div className="pt-10 pb-4 flex flex-col items-center">
-              <SvgIcon id="Error" color="danger01" />
-              <Typography
-                className="text-center mt-2 mb-2"
-                variant={'header03'}
-              >
-                {modalHeader}
-              </Typography>
-              <Typography className="text-center" variant={'body04'}>
-                {modalBody}
-              </Typography>
-            </div>
-            <ModalFooter>{footer}</ModalFooter>
-          </ModalContent>
-        </ModalWithNextui>
-      )}
+      <ModalWithNextui
+        backdrop="opaque"
+        isOpen={isOpen}
+        onOpenChange={handleOpenChange}
+        hideCloseButton={true}
+        classNames={{
+          backdrop:
+            'bg-gradient-to-t from-zinc-900 to-zinc-900/10 backdrop-opacity-20',
+        }}
+      >
+        <ModalContent className="w-[310px] m-auto">
+          <div className="pt-10 pb-4 flex flex-col items-center">
+            <SvgIcon id="Error" color="danger01" />
+            <Typography className="text-center mt-2 mb-2" variant={'header03'}>
+              네트워크 에러
+            </Typography>
+            <Typography className="text-center" variant={'body04'}>
+              {'네트워크 에러가 발생하였습니다.'}
+            </Typography>
+          </div>
+          <ModalFooter>
+            <Button>다시 시도</Button>
+          </ModalFooter>
+        </ModalContent>
+      </ModalWithNextui>
     </>
   );
 }
