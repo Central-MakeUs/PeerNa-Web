fceb6090ca1fcd248c140cfcfaf54721763fead8
sy-paik <sypaik.dev@gmail.com>
2024-02-11 18:12:54 +0900

    fix: 토큰 없을 경우 api 호출 제어

--- src/hooks/api/member/index/useGetMe.tsx
+++ src/hooks/api/member/index/useGetMe.tsx
@@ -3,6 +3,7 @@ import { QUERY_KEY } from '@constants/queryKey';
 import { useQuery } from '@tanstack/react-query';
 import { MemberDefaultInformationTypeWithUuid } from '@type/index';
 import { ApiResponse, http } from '@utils/API';
+import { getAccessToken } from '@utils/token';
 
 export interface MemberMeResponseDTO
   extends MemberDefaultInformationTypeWithUuid {}
@@ -14,11 +15,12 @@ const getMe = async (): Promise<ApiResponse<MemberMeResponseDTO>> => {
 
 export default function useGetMe() {
   const isOnboarding = localStorage.getItem(UtilityKeys.IS_ONBOARD);
+  const hasToken = getAccessToken();
 
   return useQuery({
     queryKey: [QUERY_KEY.MEMBER_ME],
     queryFn: getMe,
     select: data => data.result,
-    enabled: !isOnboarding,
+    enabled: !isOnboarding && !!hasToken,
   });
 }
