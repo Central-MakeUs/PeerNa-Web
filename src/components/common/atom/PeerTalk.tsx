2531b9aea9c83a2b8307d122b82b9429569d2f4e
sy-paik <sypaik.dev@gmail.com>
2024-02-12 23:20:06 +0900

    style: 종합평가 개수 너비 및 gap 추가

--- src/components/common/atom/PeerTalk.tsx
+++ src/components/common/atom/PeerTalk.tsx
@@ -18,13 +18,8 @@ export default function PeerTalk({
 }: TalkProps) {
   const talkStyle = 'rounded-xl pl-4 pr-2 py-2 box-content';
   return (
-    <div className="flex w-full items-center">
-      <Typography
-        variant="body01"
-        fontColor="primary"
-        as="span"
-        className="mr-3"
-      >
+    <div className="flex w-full gap-2 items-center">
+      <Typography variant="body01" fontColor="primary" className="w-[20px]">
         {count.toString()}
       </Typography>
       <ChipWithNextui
