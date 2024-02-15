0b66e47515f2a8694bdda5feb2fb683a309a27e3
sy-paik <sypaik.dev@gmail.com>
2024-02-15 18:25:24 +0900

    feat: 카카오 공유 훅 및 상수 분리

--- src/pages/home/index/atom/ReviewButton.tsx
+++ src/pages/home/index/atom/ReviewButton.tsx
@@ -1,34 +1,28 @@
 import SvgIcon from '@components/common/atom/SvgIcon';
 import Typography from '@components/common/atom/Typography';
+import { PEER_TEST_REQUEST } from '@constants/share';
 import useGetMe from '@hooks/api/member/index/useGetMe';
-import useSendKakaoMessage from '@hooks/common/useSendKakoMessage';
+import useSendKakaoMessage, {
+  PEER_TEST_URL,
+} from '@hooks/common/useSendKakoMessage';
 
 export default function ReviewButton() {
   const { data } = useGetMe();
 
   const uuid = data ? data.uuid : '';
+  const shareUrl = PEER_TEST_URL(uuid);
 
-  const handleSendKakaoMessage = useSendKakaoMessage();
-  const title = '저는 어떤 동료인가요?';
-  const description = '함께한 동료에 대해 알려주세요.';
-  const buttonText = '피어 테스트 응답하기';
-  const imagePath =
-    'https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYTMvDA1jejdkIRTg7jKQg9KDNB1yXAZ-zW7EOaTqlAjOsqxJlsG8PH9cSk5UOENxqdZzxVUdhTq8lOfccEjqeXKveLD=w1920-h868';
-  const path = `review/peer/?uuid=${uuid}`;
+  const { handleSendKakaoMessage } = useSendKakaoMessage();
+
+  const handleKakaoShare = () => {
+    handleSendKakaoMessage({ ...PEER_TEST_REQUEST, url: shareUrl });
+  };
 
   return (
     <div className="mt-8 mb-10 px-4 w-full mx-auto">
       <button
         className="bg-primary100 rounded-xl w-full flex px-4 py-5 justify-between items-center"
-        onClick={() =>
-          handleSendKakaoMessage({
-            title,
-            description,
-            buttonText,
-            imagePath,
-            path,
-          })
-        }
+        onClick={handleKakaoShare}
       >
         <div>
           <Typography variant="header03" fontColor="gray08" className="mb-1">
