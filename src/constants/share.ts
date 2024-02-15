5cd95c774b3f5660d97e457a3885eeffccec0010
sy-paik <sypaik.dev@gmail.com>
2024-02-15 18:24:11 +0900

    feat: 카카오 공유 상수 추가

---
+++ src/constants/share.ts
@@ -0,0 +1,10 @@
+import { PEER_TEST_URL } from '@hooks/common/useSendKakoMessage';
+
+export const PEER_TEST_REQUEST = {
+  title: '저는 어떤 동료인가요?',
+  description: '함께한 동료에 대해 알려주세요.',
+  buttonText: '피어테스트 응답하기',
+  imagePath:
+    'https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYTMvDA1jejdkIRTg7jKQg9KDNB1yXAZ-zW7EOaTqlAjOsqxJlsG8PH9cSk5UOENxqdZzxVUdhTq8lOfccEjqeXKveLD=w1920-h868',
+  url: PEER_TEST_URL,
+};
