7f201d0d2258f3e958a6714b18ef00df6a0893cb
sy-paik <sypaik.dev@gmail.com>
2024-02-15 18:27:36 +0900

    refactor: 불필요한 modal 훅 삭제

--- src/pages/mypage/index/molecule/SelfTestModal.tsx
+++ src/pages/mypage/index/molecule/SelfTestModal.tsx
@@ -3,44 +3,28 @@ import Typography from '@components/common/atom/Typography';
 import { useFlow } from '@hooks/common/useStackFlow';
 import useModal from '@hooks/store/useModal';
 import { Modal, ModalContent, ModalFooter } from '@nextui-org/react';
-import { useState } from 'react';
 
 export default function SelfTestModal() {
-  const { isOpen, openModal: selfTestOpen, closeModal } = useModal('selfTest');
+  const { isOpen, closeModal } = useModal('selfTest');
   const { replace } = useFlow();
-  const [preventClose, setPreventClose] = useState(false);
-
-  const handleOpenChange = (isOpen: boolean) => {
-    if (isOpen) selfTestOpen();
-  };
 
   const handleClickSelfTest = () => {
-    setPreventClose(true);
     replace('ReviewSelfPage', { step: '1' });
     closeModal();
-  };
-
-  const handleBackdropClick = () => {
-    if (preventClose) return;
   };
 
   return (
     <Modal
       isOpen={isOpen}
       backdrop="opaque"
-      onOpenChange={handleOpenChange}
       hideCloseButton={true}
-      onClose={() => setPreventClose(false)}
       classNames={{
         wrapper: 'z-[100]',
         backdrop:
           'z-[99] bg-gradient-to-t from-zinc-900 to-zinc-900/10 backdrop-opacity-20',
       }}
     >
-      <ModalContent
-        className="w-[310px] m-auto"
-        onClick={e => e.stopPropagation()}
-      >
+      <ModalContent className="w-[310px] m-auto">
         <div className="pt-10 pb-4 px-4">
           <Typography className="text-center mb-5" variant="header03">
             {'셀프테스트를 해주세요'}
@@ -61,12 +45,6 @@ export default function SelfTestModal() {
           </div>
         </ModalFooter>
       </ModalContent>
-      {isOpen && (
-        <div
-          className="fixed top-0 left-0 w-full h-full"
-          onClick={handleBackdropClick}
-        />
-      )}
     </Modal>
   );
 }
