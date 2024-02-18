bf93c70d008a205be474cf5857e9d91815dcb902
sy-paik <sypaik.dev@gmail.com>
2024-02-18 23:26:37 +0900

    style: 줄바꿈 처리 제거

--- src/pages/mypage/index/molecule/ProfileCard.tsx
+++ src/pages/mypage/index/molecule/ProfileCard.tsx
@@ -29,10 +29,14 @@ export default function ProfileCard({
       >
         <Button
           buttonVariant="tertiary"
-          className="!w-[103px] !h-[40px] !px-5 !py-2.5 flex-1 !min-w-min"
+          className="!max-w-[103px] !h-[40px] !px-5 !py-2.5 flex-1 !min-w-min"
           onClick={handleProfileEdit}
         >
-          <Typography variant="body03" className="text-gray08">
+          <Typography
+            variant="body03"
+            fontColor="gray08"
+            className="!whitespace-nowrap"
+          >
             프로필 수정
           </Typography>
         </Button>
