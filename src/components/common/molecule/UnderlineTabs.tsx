76248bf413b4ec9c4306a06938db374f2ce3ced8
mj <minai621@naver.com>
2024-02-20 02:00:00 +0900

    style: 프로젝트, 알림 페이지 간격 수정

--- src/components/common/molecule/UnderlineTabs.tsx
+++ src/components/common/molecule/UnderlineTabs.tsx
@@ -21,7 +21,7 @@ export default function UnderlineTabs({
         tabList:
           'w-full relative rounded-none p-0 border-b border-divider overflow-visible',
         cursor: 'w-full bg-[#67737E] -bottom-[1.4px]',
-        tab: 'px-4 h-12',
+        tab: 'px-4 !h-12',
         tabContent: `${FontVariantsClassName.body01} text-gray04 group-data-[selected=true]:text-gray08`,
       }}
     >
