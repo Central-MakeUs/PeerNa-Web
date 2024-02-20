87f95a245f68e8464fe283bdc4c6ab5a81226065
sy-paik <sypaik.dev@gmail.com>
2024-02-21 02:06:39 +0900

    style: margin 값 및 레이아웃 수정

--- src/components/common/molecule/ProfileListItem.tsx
+++ src/components/common/molecule/ProfileListItem.tsx
@@ -21,9 +21,9 @@ export default function ProfileListItem({
   const peerType = ICON_MAPPER[peerTestType];
   return (
     <div
-      className={`flex flex-1 gap-2 justify-between items-center ${isMyProfile ? 'w-full' : 'max-w-[524px]'} px-5 mx-auto pt-8 pb-4`}
+      className={`flex flex-1 justify-between items-center ${isMyProfile ? 'w-full' : 'max-w-[524px]'} px-5 mx-auto pt-8 pb-4`}
     >
-      <div className="flex gap-2 items-center w-[239px]">
+      <div className="flex gap-3 items-start w-[239px]">
         <div
           className={`max-w-[64px] min-w-12 max-h-[64px] min-h-12 !bg-gray01 flex items-center justify-center box-content rounded-full border-1 border-[#E3E6E8] ${isMyProfile ? 'bg-gray07' : 'bg-white'}`}
         >
@@ -36,7 +36,7 @@ export default function ProfileListItem({
             <SvgIcon id="Person" width={32} height={32} color="gray04" />
           )}
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-1">
           <div className="flex items-center gap-1">
             <Typography
               variant="body01"
